d70ac32eb377544bc188518ba20ca678e881b8fc
Joem Rafman Pelenkahu <75409382+jooemrp@users.noreply.github.com>
2023-05-20 04:08:12 +0800

    optimize img width and height for SEO

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@
 			</div>
 		</div>
 		<div class="m-auto">
-			<img class="object-fill w-full hidden xl:block" src="/img2.jpg" alt="">
+			<img class="object-fill w-full hidden xl:block" src="/img2.jpg" width="960" height="474" alt="Hero image">
 		</div>
 	</div>
 	<div class="container mx-auto mt-5">
@@ -39,7 +39,7 @@
 				<p class="text-xl text-blue-900">Our Branding services aim to create a cohesive, compelling brand identity that sets you apart from the competition and resonates with your target audience. We'll help you to create Logo Design, Brand Design & Development, Website Design, and App Development.</p>
 			</div>
 			<div class="max-w-xl">
-			<nuxt-link to="#">
+			<nuxt-link to="#" aria-current-value="CTA to Capabilities Page">
 				<button class="text-xl bg-blue-900 hover:bg-orange-600 rounded-xl px-5 py-3 text-white">Learn More</button>
 			</nuxt-link>
 			</div>
@@ -60,7 +60,7 @@
 		</div>
 	</div>
 	<div class="container mx-auto hidden xl:block">
-		<img class="object-none w-full" src="/bagan.webp" alt="">
+		<img class="object-none w-full" src="/bagan.webp" width="1209" height="483" alt="How can we help you">
 		<!-- <div class="grid grid-cols-3 gap-0 my-5">
 			<div class="mt-12">
 				<h4 class="text-3xl font-bold text-left">Share Your Issues</h4>
@@ -96,58 +96,58 @@
 	<div class="xl:grid grid-cols-2 gap-0 bg-orange-500">
 		<div class="xl:m-auto m-5">
 			<h1 class="xl:text-7xl text-5xl text-white font-bold leading-normal max-w-sm">See How We Helped Our Clients</h1>
-			<nuxt-link to="#">
+			<nuxt-link to="#" aria-current-value="CTA to Our Works Page">
 			<p class="text-2xl font-semibold text-blue-900 max-w-xl py-5">Explore Case Studies >></p>
 			</nuxt-link>
 		</div>
 		<div class="xl:w-3/5 xl:m-auto mx-5 xl:pb-0 pb-5">
-			<nuxt-link to="#">
-				<img class="xl:my-16" src="/case.png" alt="">
+			<nuxt-link to="#" aria-current-value="CTA to Our Works Page">
+				<img class="xl:my-16" src="/case.png" width="944" height="767" alt="Case studies">
 			</nuxt-link>
 		</div>
 	</div>
 	<div class="xl:h-4/5 my-14">
 		<h2 class="text-orange-500 xl:text-6xl text-5xl font-bold leading-normal text-center">Clients We've Partnered With</h2>
 		<div class="xl:grid grid-cols-4 w-3/5 mx-auto justify-items-center my-16">
-			<img class="h-3/5 w-auto xl:mt-5 mt-10" src="/a-01.webp" alt="">
-			<img class="h-3/5 w-auto xl:mt-5 mt-10" src="/a-02.webp" alt="">
-			<img class="h-3/5 w-auto xl:mt-5 mt-10" src="/a-03.webp" alt="">
-			<img class="h-3/5 w-auto xl:mt-5 mt-10" src="/a-04.webp" alt="">
+			<img class="xl:mt-5 mt-10" src="/a-01.webp" width="200" height="82" alt="Partner list biofarma">
+			<img class="xl:mt-5 mt-10" src="/a-02.webp" width="200" height="82" alt="Partner list evocare">
+			<img class="xl:mt-5 mt-10" src="/a-03.webp" width="200" height="82" alt="Partner list fufang">
+			<img class="xl:mt-5 mt-10" src="/a-04.webp" width="200" height="82" alt="Partner list genetics indonesia">
 		</div>
 	</div>
 	<div class="bg-rose-900 text-white">
 		<h1 class="text-5xl font-bold text-center pt-16 pb-10">FAQ</h1>
 		<div class="container mx-auto xl:w-3/5">
 			<div class="mx-5 py-5">
-				<h3 class="text-2xl font-bold">What kind of healthcare and pharmaceutical companies do you work with?</h3>
+				<h2 class="text-2xl font-bold">What kind of healthcare and pharmaceutical companies do you work with?</h2>
 				<p class="text-xl pt-2"> We work with a variety of healthcare and pharmaceutical companies, including hospitals, clinics, medical device manufacturers, biotech companies, and pharmaceutical manufacturers. Our clients are startups to large multinational corporations.</p>
 			</div>
 			<div class="mx-5 py-5">
-				<h3 class="text-2xl font-bold">What type of digital marketing services do you provide?</h3>
+				<h2 class="text-2xl font-bold">What type of digital marketing services do you provide?</h2>
 				<p class="text-xl pt-2"> We provide varied digital marketing services for healthcare providers, including search engine optimization (SEO), social media marketing, email marketing, pay-per-click advertising, and content marketing.</p>
 			</div>
 			<div class="mx-5 py-5">
-				<h3 class="text-2xl font-bold">How can digital marketing benefit my company? </h3>
+				<h2 class="text-2xl font-bold">How can digital marketing benefit my company? </h2>
 				<p class="text-xl pt-2"> Digital marketing helps to increase your brand visibility online, attract new targeted customers, and retain current customers. It can also improve your brand's reputation, build trust with patients, and increase patient engagement.</p>
 			</div>
 			<div class="mx-5 py-5">
-				<h3 class="text-2xl font-bold">How do you tailor your services to my specific healthcare brand? </h3>
+				<h2 class="text-2xl font-bold">How do you tailor your services to my specific healthcare brand? </h2>
 				<p class="text-xl pt-2"> We have a community of healthcare professionals (all 30,000 of them1!) and work closely with healthcare key players that trust us for informational and educational content. We are also experienced for more than 8 years in the healthcare industry, and take good care to understand your unique needs. We develop a customized digital marketing support tailored to your specific goals and needs.</p>
 			</div>
 			<div class="mx-5 py-5">
-				<h3 class="text-2xl font-bold">How do you measure the success of your digital marketing efforts?</h3>
+				<h2 class="text-2xl font-bold">How do you measure the success of your digital marketing efforts?</h2>
 				<p class="text-xl pt-2"> We use a variety of metrics to measure the success of our digital marketing efforts, including website and google my business traffic, engagement rates, conversion rates, and return on investment (ROI)</p>
 			</div>
 			<div class="mx-5 py-5">
-				<h3 class="text-2xl font-bold">Can you provide examples of successful healthcare digital marketing campaigns you have run in the past? </h3>
+				<h2 class="text-2xl font-bold">Can you provide examples of successful healthcare digital marketing campaigns you have run in the past? </h2>
 				<p class="text-xl pt-2"> Absolutely. We're proud of the work we've done for our healthcare clients and would be happy to share case studies that demonstrate our expertise and results. Click here to view case studies!</p>
 			</div>
 			<div class="mx-5 py-5">
-				<h3 class="text-2xl font-bold">What is your pricing model? </h3>
+				<h2 class="text-2xl font-bold">What is your pricing model? </h2>
 				<p class="text-xl pt-2"> Our pricing varies depending on the specific services you need and the scope of the work. We offer customized pricing packages that are tailored to your specific budget and goals.</p>
 			</div>
 			<div class="mx-5 pt-5 pb-16">
-				<h3 class="text-2xl font-bold">How do I get started?</h3>
+				<h2 class="text-2xl font-bold">How do I get started?</h2>
 				<p class="text-xl pt-2"> If you haven't, <a href="https://sipsdigital.typeform.com/SIPS-DC" target="_blank">click here to inform us about your needs.</a> We will set up an initial consultation to discuss deeply and develop a customized digital marketing strategy for you!</p>
 			</div>
 		</div>
