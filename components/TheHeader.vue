7592f87a2d606dfd3e3a55a8057342e2f6a55706
Joem Rafman Pelenkahu <75409382+jooemrp@users.noreply.github.com>
2023-05-22 10:54:57 +0800

    enable capabilities page

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -6,7 +6,7 @@
 					<img src="/sips-ico.png" class="w-3/4 hidden xl:block" alt="SIPS Digital Creative">
 				</NuxtLink>
 				<ul class="flex gap-16 items-center">
-					<li><NuxtLink to="#" aria-label="Temporary disabled until development finish">CAPABILITIES</NuxtLink></li>
+					<li><NuxtLink to="/capabilities" aria-label="Temporary disabled until development finish">CAPABILITIES</NuxtLink></li>
 					<li><NuxtLink to="#" aria-label="Temporary disabled until development finish">WORK</NuxtLink></li>
 					<li><a href="https://sipsedutech.id/en/about" target="_blank" aria-label="About company">ABOUT</a></li>
 				</ul>
