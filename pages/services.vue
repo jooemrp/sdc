718bc8e94de93ceb518f03bd7df20162ef184c81
Joem Rafman Pelenkahu <75409382+jooemrp@users.noreply.github.com>
2023-10-24 15:46:36 +0800

    update service page

--- pages/services.vue
+++ pages/services.vue
@@ -1,261 +1,163 @@
 <template>
-  <div class="justify-items-center bg-gradient-to-r from-orange-700 to-orange-500 px-5 py-20">
-    <h1 class="xl:text-7xl text-5xl text-white font-bold xl:leading-normal leading-tight xl:mx-auto max-w-6xl">
+  <div class="px-5 py-20 justify-items-center bg-gradient-to-r from-orange-700 to-orange-500">
+    <h1 class="max-w-6xl text-5xl font-bold leading-tight text-white xl:text-7xl xl:leading-normal xl:mx-auto">
       Decoding the Complexities of Healthcare Marketing for Your Business
       Success
     </h1>
-    <p class="xl:text-xl max-w-6xl mx-auto text-white pt-5 leading-loose">
+    <p class="max-w-6xl pt-5 mx-auto leading-loose text-white xl:text-xl">
       At SIPS Digital Creative, we pride ourselves on our broad range of
       capabilities designed to help your healthcare business thrive in the
       digital age. We specialize in simplifying complex marketing challenges and
       turning them into growth opportunities for our clients.
     </p>
   </div>
-  <div class="container xl:px-48 mx-auto mt-5">
-    <div class="xl:grid grid-cols-2 gap-6 space-y-10 m-5 xl:my-10">
-      <div class="row-span-6">
-        <h2 class="text-orange-500 xl:text-7xl text-5xl font-bold mt-10">
-          SEO <br />
-          Management
+  <div class="container mx-auto mt-5 xl:px-40 ">
+    <div class="grid-cols-2 gap-6 m-5 space-y-5 xl:grid xl:my-10">
+      <div class="">
+        <h2 class="mt-10 text-5xl font-bold text-orange-500 xl:text-7xl">
+          HCP Engagement
         </h2>
       </div>
       <div class="mx-auto">
-        <h3 class="text-orange-500 text-4xl font-bold mb-4">SEO Audit</h3>
-        <p class="text-xl">
-          We'll start by performing a thorough SEO audit of your website to
-          assess your current performance, identify areas of strength, and
-          uncover opportunities for improvement. This includes evaluating your
-          website's structure, content, speed, mobile-friendliness, and overall
-          user experience.
+        <!-- <h3 class="mb-3 text-3xl font-bold text-orange-500">Description</h3> -->
+        <p class="mb-5 leading-loose xl:text-xl">
+          HCP Engagement is a vital service that connects pharmaceutical companies with HCP to foster collaboration, increase awareness, and gather valuable insights. 
         </p>
+        <p class="mb-10 leading-loose xl:text-xl">
+          Our HCP program is a tailored solution designed to bridge the gap between your company and HCP. By facilitating meaningful interactions and partnerships, we enable your company to leverage the expertise and trust of HCP to build and enhance your brand recognition.
+        </p>
+        <p class="mb-3 leading-loose xl:text-xl">
+          We craft a meaningful and effective outreach strategy to engage HCP, includes various communication channels ensuring multi-faceted approach to connect with HCP:
+        </p>
+        <p class="mb-10 leading-loose xl:text-xl">- Live Webinar/CME with SKP Points
+        <br>- Email & Whatsapp Marketing
+        <br>- Survey, polling and feedback generation </p>
+        <h3 class="mb-3 text-3xl font-bold text-orange-500">Case Study</h3>
+        <div class="grid justify-around grid-cols-2 gap-5 mx-auto">
+          <div class="p-5 transition transform bg-white rounded-lg shadow-md hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none">
+            <NuxtLink to="/" target="_blank" aria-label="To homepage">
+              <img class="mx-auto" src="/a-04.webp" width="200" height="82" alt="Partner list genetics indonesia" />
+            </NuxtLink>
+          </div>
+          <div class="p-5 transition transform bg-white rounded-lg shadow-md hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none">
+            <NuxtLink to="/" target="_blank" aria-label="To homepage">
+              <img class="mx-auto" src="/a-01.webp" width="200" height="82" alt="Partner list biofarma"/>
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="flex bg-white">
+    <div class="container mx-auto mt-5 bg-white xl:px-36">
+      <div class="grid-cols-2 gap-6 m-5 space-y-10 xl:grid xl:my-10">
+        <div class="">
+          <h2 class="mt-10 text-5xl font-bold text-orange-500 xl:text-7xl">
+            Brand Awareness
+          </h2>
+        </div>
+        <div class="mx-auto">
+          <!-- <h3 class="mb-4 text-4xl font-bold text-orange-500">SEO Audit</h3> -->
+          <p class="mb-5 leading-loose xl:text-xl">
+            Our brand awareness program is a multi-faceted solution to amplify the presence of your brand across a diverse audience, including HCP and the general population.  
+          </p>
+          <p class="mb-5 leading-loose xl:text-xl">
+            This program combines and bridge interaction between HCP and general population through educating both audience, and assigning HCP to educate the general population, through:
+          </p>
+          <p class="leading-loose xl:text-xl">- Live Webinar/CME with SKP Points
+          <br>- Email & Whatsapp Marketing
+          <br>- Medical KOL Management 
+          <br>- Creation and sharing of health education content </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="px-5 text-white bg-gradient-to-br from-orange-600 to-orange-500 py-14">
+    <div class="container grid-cols-2 gap-6 mx-auto xl:space-y-10 xl:mx-auto xl:px-36 xl:grid">
+      <div class="pt-10 mx-auto">
+        <h2 class="mt-0 text-4xl font-bold text-white xl:mt-10 xl:inline xl:text-7xl">
+          Comprehensive Digital Marketing Support
+        </h2>
       </div>
       <div class="mx-auto">
-        <h3 class="text-orange-500 text-4xl font-bold mb-4">
-          Keyword Research
-        </h3>
-        <p class="text-xl">
-          Our team will conduct extensive keyword research to identify the terms
-          that your target audience is using when searching for your products or
-          services. This involves analyzing search volume, competition, and
-          relevance to ensure we focus on the most valuable keywords for your
-          business.
+        <p class="mt-3 mb-5 text-xl leading-relaxed">
+          Our comprehensive digital marketing support is a tailored solution designed to empower our clients with a 360-degree digital strategy. It is suitable for new product launches, go-to-market activation, targeting diverse audience, including HCP and the general population. 
+        </p>
+        <p class="mb-5 text-xl leading-relaxed">This program incorporates a multi-channel approach, leveraging the strength of various digital platforms, including marketing campaigns for HCP and the general population, including:</p>
+        <p class="text-xl leading-relaxed">- Live Webinar/CME with SKP Points
+        <br>- Email & Whatsapp Marketing
+        <br>- Landing page creation 
+        <br>- Ads banner placement
+        <br>- Online and offline FGD
+        <br>- Creation of educational/product information materials
+        <br>- Medical KOL Management 
+        <br>- Social Media Marketing
+        <br>- Event Production
         </p>
       </div>
-      <div class="mx-auto">
-        <h3 class="text-orange-500 text-4xl font-bold mb-4">On-Page SEO</h3>
-        <p class="text-xl">
-          We'll optimize your website's content and meta tags to improve your
-          rankings for the identified keywords. This includes optimizing title
-          tags, meta descriptions, headers, and URL structures, as well as
-          ensuring your content is high-quality and relevant to your audience.
-        </p>
+      <!-- <div class="row-span-6 mx-auto">
+        <h2 class="hidden mt-0 text-4xl font-bold text-right text-white xl:mt-10 xl:inline xl:text-7xl">
+          Comprehensive Digital Marketing Support
+        </h2>
+      </div>       -->
+    </div>
+  </div>
+
+  <div class="container mx-auto mt-5 xl:px-36">
+    <div class="grid-cols-2 gap-6 m-5 space-y-10 xl:grid">
+      <div class="">
+        <h2 class="mt-10 text-5xl font-bold text-rose-900 xl:text-7xl">
+          Market Research
+        </h2>
       </div>
       <div class="mx-auto">
-        <h3 class="text-orange-500 text-4xl font-bold mb-4">Local SEO</h3>
-        <p class="text-xl">
-          Our Branding services aim to create a cohesive, compelling brand
-          identity that sets you apart from the competition and resonates with
-          your target audience. We'll help you to create Logo Design, Brand
-          Design & Development, Website Design, and App Development.
+        <p class="mb-5 text-xl leading-relaxed">
+          Our market research program is a comprehensive and adaptable solution designed to help your brand gain a deep understanding of your target audience, market trends, and competitive landscape. By employing a mix of qualitative and quantitative research methods, we empower your company to make informed decisions, optimize product development, and enhance market strategies, all while reaching both HCP and the general population. 
         </p>
-      </div>
-      <div class="mx-auto">
-        <h3 class="text-orange-500 text-4xl font-bold mb-4">
-          Content Creation
-        </h3>
-        <p class="text-xl">
-          Our team will create high-quality, SEO-optimized content for your
-          website. This might include blog posts, articles, infographics, and
-          videos, all designed to engage your audience, provide value, and
-          improve your search engine rankings.
-        </p>
-      </div>
-      <div class="mx-auto">
-        <h3 class="text-orange-500 text-4xl font-bold mb-4">Link Building</h3>
-        <p class="text-xl pb-10">
-          We'll conduct a link-building campaign to increase the authority of
-          your website in the eyes of search engines. This involves earning
-          high-quality backlinks from reputable, relevant websites, which can
-          significantly improve your search engine rankings.
+        <p class="mb-5 text-xl leading-relaxed">What we do:</p>
+        <p class="text-xl leading-relaxed">- Needs assessment and audience segmentation
+        <br>- Surveys and questionnaires
+        <br>- Interviews and Focus group discussion 
+        <br>- Data collection and analysis
+        <br>- Market trends and competitor analysis
+        <br>- Comprehensive reporting 
         </p>
       </div>
     </div>
   </div>
-  <div class="bg-rose-900 text-white py-14 px-5">
-    <div class="xl:mx-auto xl:px-48 container xl:grid grid-cols-2 gap-6 space-y-10 mx-auto">
-      <div class="mx-auto xl:block hidden">
-        <h3 class="text-4xl font-bold mb-4 mt-10">Strategy Development</h3>
-        <p class="text-xl">
-          We start by crafting a tailored paid advertising strategy. This
-          includes identifying the best platforms for your ads, defining your
-          target audience, setting objectives, and deciding on budget
-          allocation.
-        </p>
-      </div>
-      <div class="row-span-6 mx-auto">
-        <h2 class="xl:text-7xl text-5xl font-bold xl:text-right text-left">
-          Paid Marketing<br />
-          (Ads)
-        </h2>
-      </div>
-      <div class="mx-auto xl:hidden block">
-        <h3 class="text-4xl font-bold mb-4 mt-10">Strategy Development</h3>
-        <p class="text-xl">
-          We start by crafting a tailored paid advertising strategy. This
-          includes identifying the best platforms for your ads, defining your
-          target audience, setting objectives, and deciding on budget
-          allocation.
-        </p>
-      </div>
-      <div class="mx-auto">
-        <h3 class="text-4xl font-bold mb-4">Ad Creation</h3>
-        <p class="text-xl">
-          Our team of creative professionals designs compelling ads that grab
-          attention and motivate viewers to take action. We ensure that every ad
-          aligns with your brand and resonates with your target audience.
-        </p>
-      </div>
-      <div class="mx-auto">
-        <h3 class="text-4xl font-bold mb-4">Campaign Management</h3>
-        <p class="text-xl">
-          We manage your ad campaigns from start to finish. This includes
-          setting up the campaigns, monitoring their performance, making
-          necessary adjustments, and managing bids and budgets to optimize
-          performance.
-        </p>
-      </div>
-      <div class="mx-auto">
-        <h3 class="text-4xl font-bold mb-4">
-          Performance Tracking and Optimization
-        </h3>
-        <p class="text-xl">
-          We track key metrics like impressions, clicks, and conversions to
-          measure the effectiveness of your campaigns. Based on these insights,
-          we continuously optimize your campaigns for maximum return on ad
-          spend.
-        </p>
-      </div>
-      <div class="mx-auto">
-        <h3 class="text-4xl font-bold mb-4">Reporting and Analysis</h3>
-        <p class="text-xl">
-          We provide regular, detailed reports on your ad performance. This
-          includes insights on your return on ad spend, conversion rates, and
-          recommendations for future campaign improvements.
-        </p>
-      </div>
-    </div>
-  </div>
-  <div class="container xl:px-48 mx-auto mt-5">
-    <div class="xl:grid grid-cols-2 gap-6 space-y-10 m-5">
-      <div class="row-span-6">
-        <h2 class="text-rose-900 xl:text-7xl text-5xl font-bold mt-10">
-          Content <br />
-          Marketing
+  
+  <div class="px-5 text-white bg-gradient-to-br from-rose-900 to-rose-500 py-14">
+    <div class="container grid-cols-2 gap-6 mx-auto xl:space-y-10 xl:mx-auto xl:px-36 xl:grid">
+      <div class="pt-10 mx-auto">
+        <h2 class="mt-0 text-4xl font-bold text-white xl:mt-10 xl:inline xl:text-7xl">
+          Technology Enhancement & Digital Content Management
         </h2>
       </div>
       <div class="mx-auto">
-        <h3 class="text-rose-900 text-4xl font-bold mb-4">Content Strategy</h3>
-        <p class="text-xl">
-          We start by devising a content strategy that aligns with your business
-          goals and audience's needs. This includes identifying key topics,
-          content formats, and distribution channels.
+        <p class="mt-3 mb-5 text-xl leading-relaxed">We will support you in modernizing your company's online presence and ensuring you effectively communicate your company's value to key stakeholders. Either you use it for internal content management, educating your HCP stakeholders, or as a simple corporate website, we are available for your technology enhancement. </p>
+        <p class="text-xl leading-relaxed">- Website development for CMS, LMS, and Corporate Site
+        <br>- SEO Management
+        <br>- Content Management 
+        <br>- Creation of digitize content: videos, animations, e-books and e-brochures
         </p>
       </div>
-      <div class="mx-auto">
-        <h3 class="text-rose-900 text-4xl font-bold mb-4">Content Creation</h3>
-        <p class="text-xl">
-          Our team of skilled writers and designers produce high-quality
-          content, from informative articles and engaging blog posts to
-          eye-catching infographics and compelling videos.
-        </p>
-      </div>
-      <div class="mx-auto">
-        <h3 class="text-rose-900 text-4xl font-bold mb-4">
-          Content Distribution
-        </h3>
-        <p class="text-xl">
-          We ensure your content reaches your target audience through the right
-          channels. This could include your website, social media platforms,
-          email newsletters, and more.
-        </p>
-      </div>
-      <div class="mx-auto">
-        <h3 class="text-rose-900 text-4xl font-bold mb-4">
-          Social Media Promotion
-        </h3>
-        <p class="text-xl">
-          We extend the reach of your content by sharing it on relevant social
-          media platforms, fostering engagement and encouraging shares.
-        </p>
-      </div>
-      <div class="mx-auto">
-        <h3 class="text-rose-900 text-4xl font-bold mb-4">
-          Performance Tracking and Optimization
-        </h3>
-        <p class="text-xl">
-          We use analytics to monitor how your content is performing in terms of
-          engagement, reach, and conversions. Based on these insights, we
-          continually refine your content strategy for improved results.
-        </p>
-      </div>
-      <div class="mx-auto">
-        <h3 class="text-rose-900 text-4xl font-bold mb-4">
-          Reporting and Analysis
-        </h3>
-        <p class="text-xl pb-10">
-          We provide regular reports detailing your content's performance and
-          the value it's providing to your business, helping to inform future
-          content marketing decisions.
-        </p>
-      </div>
-    </div>
-  </div>
-  <div class="bg-orange-500 text-white pt-5 pb-5 px-5">
-    <div class="xl:mx-auto xl:px-48 container xl:grid grid-cols-2 gap-6 space-y-10 mx-auto">
-      <div class="mx-auto xl:block hidden">
-        <h3 class="text-4xl font-bold mb-4 mt-10">Logo Design</h3>
-        <p class="text-xl">
-          We'll create a distinctive logo that encapsulates your brand's identity and leaves a lasting impression. 
-        </p>
-      </div>
-      <div class="row-span-6">
-        <h2 class="xl:text-7xl text-5xl font-bold xl:text-right text-left">
-          Branding
+      <!-- <div class="row-span-6 mx-auto">
+        <h2 class="hidden mt-0 text-4xl font-bold text-right text-white xl:mt-10 xl:inline xl:text-7xl">
+          Comprehensive Digital Marketing Support
         </h2>
-      </div>
-      <div class="mx-auto xl:hidden block">
-        <h3 class="text-4xl font-bold mb-4 mt-10">Logo Design</h3>
-        <p class="text-xl">
-          We'll create a distinctive logo that encapsulates your brand's identity and leaves a lasting impression. 
-        </p>
-      </div>
-      <div class="mx-auto">
-        <h3 class="text-4xl font-bold mb-4">Brand Design & Development</h3>
-        <p class="text-xl">
-          Our team will develop a comprehensive brand identity, including color schemes, typography, and visual elements, that conveys your brand's personality and values. 
-        </p>
-      </div>
-      <div class="mx-auto">
-        <h3 class="text-4xl font-bold mb-4">Website Design</h3>
-        <p class="text-xl">
-          We'll design a user-friendly, visually appealing website that aligns with your brand identity and serves as a powerful marketing tool. 
-        </p>
-      </div>
-      <div class="mx-auto">
-        <h3 class="text-4xl font-bold mb-4">App Development</h3>
-        <p class="text-xl pb-10">
-          If your business needs a mobile app, our team will design and develop a functional, user-friendly app that provides value to your customers and aligns with your brand. 
-        </p>
-      </div>
+      </div>       -->
     </div>
   </div>
-  <div class="flex flex-col items-center justify-center py-14 w-full">
+
+  <div class="flex flex-col items-center justify-center w-full py-14">
     <a
       href="https://sipsdigital.typeform.com/SIPS-DC"
       aria-label="CTA to Typeform"
     >
       <button
-        class="text-xl bg-green-600 hover:bg-blue-900 rounded-xl px-5 py-3 text-white"
+        class="px-5 py-3 text-xl text-white bg-green-600 hover:bg-blue-900 rounded-xl"
       >
         Request Your Tailored Solution
       </button>
