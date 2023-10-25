62766b024cc2ad8dbf06d40d57fd101e8e4fb3d8
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2023-10-25 10:45:04 +0800

    Update [id].vue

--- pages/works/[id].vue
+++ pages/works/[id].vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="h-50" :class="`flex items-center bg-gradient-to-br pt-28 md:pb-20 md:pt-52 bg-${ getRandomColor() }`">
+    <section id="h-50" :class="`flex items-center bg-gradient-to-br pt-28 md:pb-20 md:pt-52 bg-${getRandomColor()}`">
         <div class="container mx-auto text-white xl:p-0 p-5">
             <h1 class="xl:text-5xl text-4xl font-bold max-w-4xl">
                 {{ work?.title }}
@@ -8,7 +8,22 @@
                 {{ work?.category }}
             </p>
         </div>
-    </div>
+    </section>
+
+    <section class="w-4/5 m-20 mx-auto">
+
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <h3 class="text-xl font-semibold text-sips-navy mb-2 md:mb-0">The Challenge</h3>
+            <p class="text-sips-navy">{{ work?.challenge }}</p>
+
+            <h3 class="text-xl font-semibold text-sips-navy mt-7 md:mt-10 mb-2 md:mb-0">Our Approach</h3>
+            <p class="text-sips-navy md:mt-10">{{ work?.approach }}</p>
+
+            <h3 class="text-xl font-semibold text-sips-navy mt-7  md:mt-10 mb-2 md:mb-0">The Result</h3>
+            <p class="text-sips-navy md:mt-10">{{ work?.result }}</p>
+        </div>
+
+    </section>
 </template>
 
 <script setup>
@@ -21,6 +36,7 @@ const work = ref(null);
 
 useFetch(`https://api.sipsedutech.id/api/works/${id}`).then(res => {
     work.value = res.data.value.data
+    console.log(work)
 }, error => {
     console.log(error)
 })
