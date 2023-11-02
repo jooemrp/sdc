c8219dca7c619324b18a76bd6556e61de47b9fce
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2023-11-02 16:32:13 +0800

    Update services.vue

--- pages/services.vue
+++ pages/services.vue
@@ -1,16 +1,21 @@
 <template>
     <div
-        class="flex justify-center pt-24 pb-24 mx-auto bg-gradient-to-r from-orange-700 to-orange-500"
+        class="mx-auto flex justify-center bg-gradient-to-r from-orange-700 to-orange-500 pb-24 pt-24"
     >
         <div class="container text-center">
             <h1
                 class="mx-5 mb-5 text-3xl font-bold text-white xl:text-7xl xl:leading-snug"
+                data-aos="fade-up"
+                data-aos-duration="1000"
             >
                 Achieve your pharmaceutical goals with our proven digital
                 marketing strategy that maximizes benefits
             </h1>
             <p
-                class="w-3/4 mx-auto mt-10 mb-5 leading-relaxed text-white xl:text-3xl"
+                class="mx-auto mb-5 mt-10 w-3/4 leading-relaxed text-white xl:text-3xl"
+                data-aos="fade-up"
+                data-aos-duration="1500"
+                data-aos-delay="300"
             >
                 Uncover the path to achieving your pharmaceutical goals with our
                 proven digital marketing strategy, designed to maximize
@@ -21,15 +26,13 @@
     </div>
 
     <section class="flex justify-center pt-12 xl:mt-10 xl:px-36">
-        <div class="container grid-cols-2 gap-6 mx-5 xl:grid">
+        <div class="container mx-5 grid-cols-2 gap-6 xl:grid">
             <div class="mb-8">
-                <h2
-                    class="text-4xl font-bold text-orange-500 xl:text-6xl"
-                >
+                <h2 class="text-4xl font-bold text-orange-500 xl:text-6xl">
                     HCP <br />Engagement
                 </h2>
                 <img
-                    class="hidden mt-36 h-1/2 xl:block"
+                    class="mt-36 hidden h-1/2 xl:block"
                     src="/sips-digital-hcp.webp"
                     alt="SIPS Digital HCP Service"
                 />
@@ -52,18 +55,20 @@
                     engage HCP, includes various communication channels ensuring
                     multi-faceted approach to connect with HCP:
                 </p>
-                <ul class="mb-10 leading-relaxed list-disc list-inside xl:text-xl">
+                <ul
+                    class="mb-10 list-inside list-disc leading-relaxed xl:text-xl"
+                >
                     <li>Live Webinar/CME with SKP Points</li>
                     <li>Email & Whatsapp Marketing</li>
                     <li>Survey, polling and feedback generation</li>
                 </ul>
 
                 <h3 class="mb-8 text-2xl font-bold text-orange-500 xl:text-3xl">
-                  See how we've done it with...
+                    See how we've done it with...
                 </h3>
-                <div class="grid justify-around grid-cols-2 gap-5 mx-auto">
+                <div class="mx-auto grid grid-cols-2 justify-around gap-5">
                     <div
-                        class="p-5 transition transform bg-white rounded-lg shadow-md hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none"
+                        class="transform rounded-lg bg-white p-5 shadow-md transition hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none"
                     >
                         <NuxtLink
                             to="/"
@@ -80,12 +85,9 @@
                         </NuxtLink>
                     </div>
                     <div
-                        class="p-5 transition transform bg-white rounded-lg shadow-md hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none"
+                        class="transform rounded-lg bg-white p-5 shadow-md transition hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none"
                     >
-                        <NuxtLink
-                            to="/works/biofarma"
-                            aria-label="To homepage"
-                        >
+                        <NuxtLink to="/works/biofarma" aria-label="To homepage">
                             <img
                                 class="mx-auto"
                                 src="/a-01.webp"
@@ -100,8 +102,10 @@
         </div>
     </section>
 
-    <section class="flex justify-center pt-12 bg-sips-maroon xl:px-36">
-        <div class="container grid-cols-2 gap-6 mx-5 text-white xl:mt-16 xl:grid">
+    <section class="flex justify-center bg-sips-maroon pt-12 xl:px-36">
+        <div
+            class="container mx-5 grid-cols-2 gap-6 text-white xl:mt-16 xl:grid"
+        >
             <div class="mb-8 xl:mb-0">
                 <h2 class="mb-12 text-4xl font-bold xl:text-6xl">
                     Brand <br />Awareness
@@ -121,7 +125,9 @@
                     general population through educating both audience, and
                     assigning HCP to educate the general population, through:
                 </p>
-                <ul class="mb-10 leading-relaxed list-disc list-inside xl:text-xl">
+                <ul
+                    class="mb-10 list-inside list-disc leading-relaxed xl:text-xl"
+                >
                     <li>Live Webinar/CME with SKP Points</li>
                     <li>Email & Whatsapp Marketing</li>
                     <li>Medical KOL Management</li>
@@ -132,13 +138,13 @@
     </section>
 
     <section class="flex justify-center pt-12 xl:mt-10 xl:px-36">
-        <div class="container grid-cols-2 gap-6 mx-5 xl:grid">
+        <div class="container mx-5 grid-cols-2 gap-6 xl:grid">
             <div class="mb-8 xl:mb-0">
                 <h2 class="text-4xl font-bold text-orange-500 xl:text-6xl">
                     Comprehensive <br />Digital Marketing <br />Support
                 </h2>
                 <img
-                    class="hidden mt-32 h-2/4 xl:block"
+                    class="mt-32 hidden h-2/4 xl:block"
                     src="/sips-digital-marketing-support.webp"
                     alt="SIPS Digital Marketing Support Service"
                 />
@@ -157,11 +163,15 @@
                     including marketing campaigns for HCP and the general
                     population, including:
                 </p>
-                <ul class="mb-10 leading-relaxed list-disc list-inside xl:text-xl">
+                <ul
+                    class="mb-10 list-inside list-disc leading-relaxed xl:text-xl"
+                >
                     <li>Live Webinar/CME with SKP Points</li>
                     <li>Email & Whatsapp Marketing</li>
                     <li>Landing page creation</li>
-                    <li>Creation of educational/product information materials</li>
+                    <li>
+                        Creation of educational/product information materials
+                    </li>
                     <li>Medical KOL Management</li>
                     <li>Social Media Marketing</li>
                     <li>Event Production</li>
@@ -170,15 +180,17 @@
         </div>
     </section>
 
-    <section class="flex justify-center pt-12 xl:mt-10 xl:px-36 bg-sips-orange">
-        <div class="container grid-cols-2 gap-6 mx-5 text-white xl:mt-16 xl:grid">
+    <section class="flex justify-center bg-sips-orange pt-12 xl:mt-10 xl:px-36">
+        <div
+            class="container mx-5 grid-cols-2 gap-6 text-white xl:mt-16 xl:grid"
+        >
             <div class="mb-8 xl:mb-0">
                 <h2 class="text-4xl font-bold xl:text-6xl">
                     Market <br />
                     Research
                 </h2>
                 <img
-                    class="hidden mt-28 h-1/2 xl:block"
+                    class="mt-28 hidden h-1/2 xl:block"
                     src="/sips-digital-market-research.webp"
                     alt="SIPS Digital Market Research Service"
                 />
@@ -195,7 +207,9 @@
                     the general population.
                 </p>
                 <p class="leading-relaxed xl:text-xl">What we do:</p>
-                <ul class="mb-10 leading-relaxed list-disc list-inside xl:text-xl">
+                <ul
+                    class="mb-10 list-inside list-disc leading-relaxed xl:text-xl"
+                >
                     <li>Needs assessment and audience segmentation</li>
                     <li>Surveys and questionnaires</li>
                     <li>Interviews and Focus group discussion</li>
@@ -207,39 +221,55 @@
         </div>
     </section>
 
-  <section class="flex justify-center pt-12 xl:mt-10 xl:px-36">
-    <div class="container grid-cols-2 gap-6 mx-5 xl:grid">
-      <div class="h-auto mb-8 xl:mb-0">
-        <h2 class="text-4xl font-bold text-orange-500 xl:text-6xl">
-          Technology Enhancement & Digital Content Management
-        </h2>
-        <img class="hidden mt-28 xl:block h-2/5" src="/sips-digital-technology-digital-enhancement.webp" alt="SIPS Digital Technology Enhancement & Digital Content Management Service"/>
-      </div>
-      <div class="h-auto mx-auto">
-        <p class="mt-5 mb-5 leading-relaxed xl:text-xl">
-          We will support you in modernizing your company's online presence and ensuring you effectively communicate your company's value to key stakeholders. Either you use it for internal content management, educating your HCP stakeholders, or as a simple corporate website, we are available for your technology enhancement.
-        </p>
-        <ul class="mb-10 leading-relaxed list-disc list-inside xl:text-xl">
-          <li>Website development for CMS, LMS, and Corporate Site</li>
-          <li>SEO Management</li>
-          <li>Content Management</li>
-          <li>Creation of digitize content: videos, animations, e-books and e-brochures</li>
-        </ul>
-      </div>
+    <section class="flex justify-center pt-12 xl:mt-10 xl:px-36">
+        <div class="container mx-5 grid-cols-2 gap-6 xl:grid">
+            <div class="mb-8 h-auto xl:mb-0">
+                <h2 class="text-4xl font-bold text-orange-500 xl:text-6xl">
+                    Technology Enhancement & Digital Content Management
+                </h2>
+                <img
+                    class="mt-28 hidden h-2/5 xl:block"
+                    src="/sips-digital-technology-digital-enhancement.webp"
+                    alt="SIPS Digital Technology Enhancement & Digital Content Management Service"
+                />
+            </div>
+            <div class="mx-auto h-auto">
+                <p class="mb-5 mt-5 leading-relaxed xl:text-xl">
+                    We will support you in modernizing your company's online
+                    presence and ensuring you effectively communicate your
+                    company's value to key stakeholders. Either you use it for
+                    internal content management, educating your HCP
+                    stakeholders, or as a simple corporate website, we are
+                    available for your technology enhancement.
+                </p>
+                <ul
+                    class="mb-10 list-inside list-disc leading-relaxed xl:text-xl"
+                >
+                    <li>
+                        Website development for CMS, LMS, and Corporate Site
+                    </li>
+                    <li>SEO Management</li>
+                    <li>Content Management</li>
+                    <li>
+                        Creation of digitize content: videos, animations,
+                        e-books and e-brochures
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <div class="mb-8 flex w-full flex-col items-center justify-center pb-8">
+        <a
+            href="https://form.typeform.com/to/wrXbzSpp"
+            aria-label="CTA to Typeform"
+        >
+            <button
+                class="rounded-xl bg-green-600 px-5 py-3 text-xl text-white hover:bg-blue-900"
+            >
+                Talk to us
+            </button>
+        </a>
     </div>
-  </section>
-    <div class="flex flex-col items-center justify-center w-full pb-8 mb-8">
-    <a
-      href="https://form.typeform.com/to/wrXbzSpp"
-      aria-label="CTA to Typeform"
-    >
-      <button
-        class="px-5 py-3 text-xl text-white bg-green-600 hover:bg-blue-900 rounded-xl"
-      >
-        Talk to us
-      </button>
-    </a>
-  </div>
 </template>
 
 <script setup>
@@ -251,21 +281,23 @@ useHead({
         {
             hid: 'title',
             name: 'title',
-            content: 'Services | SIPS Digital Creative - Customized Solution for Pharmaceutical Marketing',
+            content:
+                'Services | SIPS Digital Creative - Customized Solution for Pharmaceutical Marketing',
         },
         {
             hid: 'description',
             name: 'description',
-            content: 'Explore our comprehensive services at SIPS Digital Creative: HCPs Engagement, Brand Awareness, Market Research, Comprehensive Digital Marketing Support, Technology Enhancement, Digital Content Management',
+            content:
+                'Explore our comprehensive services at SIPS Digital Creative: HCPs Engagement, Brand Awareness, Market Research, Comprehensive Digital Marketing Support, Technology Enhancement, Digital Content Management',
         },
     ],
     link: [
         {
-            hid:'canonical',
-            rel:'canonical',
-            href:'https://digital.sipsedutech.id/services'
+            hid: 'canonical',
+            rel: 'canonical',
+            href: 'https://digital.sipsedutech.id/services',
         },
-    ]
+    ],
 });
 </script>
 
