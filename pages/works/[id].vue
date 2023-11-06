2aa591dbbe8b10a99d6ce6147025f4bba737cd05
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2023-11-06 18:59:05 +0800

    Update [id].vue

--- pages/works/[id].vue
+++ pages/works/[id].vue
@@ -5,38 +5,47 @@
             (work?.color ? `bg-${work.color}` : 'bg-' + getRandomColor())
         "
     >
-        <div class="container p-5 mx-auto text-white xl:p-0">
-            <h1 class="max-w-4xl text-4xl font-bold xl:text-5xl">
-                {{ work?.title }}
-            </h1>
-            <p
-                class="max-w-5xl py-4 text-2xl md:py-8 md:text-3xl md:leading-normal"
-            >
-                {{ work?.category }}
-            </p>
+        <div class="container mx-auto w-4/5 py-5 text-white xl:p-0">
+            <div v-if="work.title">
+                <h1 class="max-w-4xl text-4xl font-bold xl:text-5xl">
+                    {{ work?.title }}
+                </h1>
+                <p
+                    class="max-w-5xl py-4 text-2xl md:py-8 md:text-3xl md:leading-normal"
+                >
+                    {{ work?.category }}
+                </p>
+            </div>
+            <div v-else>
+                <SharedLoader />
+            </div>
         </div>
     </section>
 
-    <section class="w-4/5 m-20 mx-auto">
-        <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
+    <section class="container m-20 mx-auto w-4/5">
+        <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
             <h3 class="mb-2 text-xl font-semibold text-sips-navy md:mb-0">
                 The Challenge
             </h3>
-            <p class="text-sips-navy">{{ work?.challenge }}</p>
+            <p class="text-sips-navy md:col-span-2">{{ work?.challenge }}</p>
 
             <h3
-                class="mb-2 text-xl font-semibold mt-7 text-sips-navy md:mb-0 md:mt-10"
+                class="mb-2 mt-7 text-xl font-semibold text-sips-navy md:mb-0 md:mt-10"
             >
                 Our Approach
             </h3>
-            <p class="text-sips-navy md:mt-10">{{ work?.approach }}</p>
+            <p class="text-sips-navy md:col-span-2 md:mt-10">
+                {{ work?.approach }}
+            </p>
 
             <h3
-                class="mb-2 text-xl font-semibold mt-7 text-sips-navy md:mb-0 md:mt-10"
+                class="mb-2 mt-7 text-xl font-semibold text-sips-navy md:mb-0 md:mt-10"
             >
                 The Result
             </h3>
-            <p class="text-sips-navy md:mt-10">{{ work?.result }}</p>
+            <p class="text-sips-navy md:col-span-2 md:mt-10">
+                {{ work?.result }}
+            </p>
         </div>
     </section>
 </template>
@@ -48,7 +57,11 @@ export default {
     name: 'Our Work',
     data() {
         return {
-            work: {},
+            work: {
+                challenge: 'Loading...',
+                approach: 'Loading...',
+                result: 'Loading...',
+            },
         };
     },
     async mounted() {
@@ -79,11 +92,11 @@ export default {
                 ],
                 link: [
                     {
-                        hid:'canonical',
-                        rel:'canonical',
-                        href:`https://digital.sipsedutech.id/works/${this.work.slug}`
+                        hid: 'canonical',
+                        rel: 'canonical',
+                        href: `https://digital.sipsedutech.id/works/${this.work.slug}`,
                     },
-                ]
+                ],
             });
         },
         getData: async function (id) {
