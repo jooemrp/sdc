451b5a092cd7a2c2248ffc941c53adc6f228ba71
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2023-11-06 19:51:06 +0800

    Add carousel to our works field
    Won't work with only 1 image content

--- pages/works/[id].vue
+++ pages/works/[id].vue
@@ -48,10 +48,122 @@
             </p>
         </div>
     </section>
+
+    <section
+        :class="
+            `flex items-center py-5 md:py-20 ` +
+            (work?.color ? `bg-${work.color}` : 'bg-sips-navy')
+        "
+        v-if="work.content_url.length > 1"
+    >
+        <div
+            id="default-carousel"
+            class="container relative mx-auto w-4/5"
+            data-carousel="slide"
+        >
+            <!-- Carousel wrapper -->
+            <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                <!-- Item 1 -->
+                <div
+                    class="hidden duration-700 ease-in-out"
+                    data-carousel-item
+                    v-for="(item, index) in work.content_url"
+                >
+                    <img
+                        :src="item"
+                        class="absolute left-1/2 top-1/2 block w-full -translate-x-1/2 -translate-y-1/2"
+                        alt="..."
+                    />
+                </div>
+                <!-- Item 2 -->
+                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                    <img
+                        src="https://digital.sipsedutech.id/what-we-do-at-sips-digital-creative.webp"
+                        class="absolute left-1/2 top-1/2 block w-full -translate-x-1/2 -translate-y-1/2"
+                        alt="..."
+                    />
+                </div>
+            </div>
+            <!-- Slider indicators -->
+            <div
+                class="absolute bottom-5 left-1/2 z-30 flex -translate-x-1/2 space-x-3"
+            >
+                <button
+                    type="button"
+                    class="h-3 w-3 rounded-full"
+                    v-for="(item, index) in work.content_url"
+                    aria-current="true"
+                    :aria-label="`Slide ${index + 1}`"
+                    :data-carousel-slide-to="index"
+                ></button>
+                <button
+                    type="button"
+                    class="h-3 w-3 rounded-full"
+                    aria-current="false"
+                    aria-label="Slide 2"
+                    data-carousel-slide-to="1"
+                ></button>
+            </div>
+            <!-- Slider controls -->
+            <button
+                type="button"
+                class="group absolute left-0 top-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none"
+                data-carousel-prev
+            >
+                <span
+                    class="inline-flex h-10 w-10 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70"
+                >
+                    <svg
+                        class="h-4 w-4 text-white dark:text-gray-800"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 6 10"
+                    >
+                        <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M5 1 1 5l4 4"
+                        />
+                    </svg>
+                    <span class="sr-only">Previous</span>
+                </span>
+            </button>
+            <button
+                type="button"
+                class="group absolute right-0 top-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none"
+                data-carousel-next
+            >
+                <span
+                    class="inline-flex h-10 w-10 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70"
+                >
+                    <svg
+                        class="h-4 w-4 text-white dark:text-gray-800"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 6 10"
+                    >
+                        <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="m1 9 4-4-4-4"
+                        />
+                    </svg>
+                    <span class="sr-only">Next</span>
+                </span>
+            </button>
+        </div>
+    </section>
 </template>
 
 <script>
 import { getRandomColor } from '@/utils/helpers';
+import { initFlowbite } from 'flowbite';
 
 export default {
     name: 'Our Work',
@@ -61,14 +173,20 @@ export default {
                 challenge: 'Loading...',
                 approach: 'Loading...',
                 result: 'Loading...',
+                content_url: [],
             },
         };
     },
     async mounted() {
         const route = useRoute();
         const id = route.params.id;
-
         this.getData(id);
+
+        // initialize components based on data attribute selectors
+        setTimeout(() => {
+            initFlowbite();
+            console.log('initFlowbite');
+        }, 1500);
     },
     methods: {
         setMeta() {
@@ -103,6 +221,7 @@ export default {
             await fetch(`https://api.sipsedutech.id/api/works/${id}`)
                 .then((response) => response.json())
                 .then((data) => {
+                    console.log('data.data', data.data);
                     this.work = data.data;
                     this.setMeta();
                 });
