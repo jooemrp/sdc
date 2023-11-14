20a0d9d7946dfee3206e9aa1995d4fb9d859d126
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2023-11-14 09:24:32 +0800

    update thank you page
    fix social share and show email address

--- pages/thankyou.vue
+++ pages/thankyou.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="h-50" class="flex items-center">
-        <div class="mx-auto xl:w-2/4 w-3/4 justify-center text-center">
+        <div class="mx-auto w-3/4 justify-center text-center xl:w-2/4">
             <img src="~/assets/tick.svg" class="mx-auto mb-12" />
-            <div class="mb-10 xl:text-6xl text-4xl font-bold">
+            <div class="mb-10 text-4xl font-bold xl:text-6xl">
                 <span
                     class="bg-gradient-to-r from-orange-500 to-orange-400 bg-clip-text text-transparent"
                 >
@@ -10,23 +10,28 @@
                 </span>
             </div>
             <div class="mb-10">
-                <p class="xl:text-3xl leading-relaxed">
-                    File e-book telah terkirim ke email Anda, cek inbox dan
-                    selamat menambah insight digital marketing Anda.
+                <p class="leading-relaxed xl:text-3xl">
+                    File Anda telah terkirim ke {{ email ?? 'Email' }}, cek
+                    inbox dan selamat menambah insight digital marketing Anda.
                 </p>
             </div>
-            <!-- <div class="mb-10">
-                <p class="text-3xl leading-relaxed">Bagikan ke teman Anda.</p>
+            <div class="">
+                <p class="text-xl leading-relaxed md:text-3xl">
+                    Bagikan ke teman Anda.
+                </p>
             </div>
-            <div class="mx-auto mb-10">
-                X X X
-                <SharedSocialShareButton class="mt-4 md:mt-5 lg:mt-7" :url="currentURL" :text="`Baca artikel`" />
-            </div> -->
+            <div class="mx-auto mb-10 flex justify-center">
+                <SharedSocialShareButton
+                    class="mt-4 md:mt-5 lg:mt-7"
+                    :url="linkToShare"
+                    :text="`Baca artikel`"
+                />
+            </div>
             <h3
                 class="mb-8 mt-5 animate-pulse pt-5 text-xl font-bold text-sips-green hover:underline xl:text-3xl"
             >
                 <NuxtLink to="/works" aria-label="To Works page">
-                    Kembali ke Insight >>
+                    &lt;&lt; Kembali ke Insight
                 </NuxtLink>
             </h3>
         </div>
@@ -34,6 +39,12 @@
 </template>
 
 <script setup>
+const route = useRoute();
+
+const { linkToShare, email } = route.query;
+
+console.log(linkToShare, email);
+
 useSeoMeta({
     title: 'Thank you for downloading the e-book, see you!',
     ogTitle: 'Thank you for downloading the e-book, see you!',
