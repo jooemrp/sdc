3983dd5d21bf0ef8c2d57cc28c5dd0e78fde94ac
Joem Rafman Pelenkahu <75409382+jooemrp@users.noreply.github.com>
2024-02-06 15:07:18 +0800

    add sips-kol pdf

---
+++ pages/sips-kol.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container block mx-auto py-24 xl:w-[800] w-[200]">
+    <iframe
+      src="/downloads/SIPS Digital KOL Management.pdf#zoom=60#toolbar=0"
+      id="pdf"
+      class="mx-auto"
+    ></iframe>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+#pdf {
+  width: 80%;
+  height: 80vh;
+}
+</style>
